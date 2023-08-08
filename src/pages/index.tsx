c7923010fdb4fcd24ad1dae5ea2c9be8e9945d32
Lukementon <lukementon15@gmail.com>
2023-08-08 07:49:25 +0100

    show error message if no meals in search results

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,10 +16,18 @@ interface Props {
 
 export default function Home({ categories }: Props) {
   const [meals, setMeals] = useState<Meal[]>();
+  const [errorMessage, setErrorMessage] = useState<string>();
+
   async function searchRecipies(queryString: string) {
+    if (Boolean(errorMessage)) {
+      setErrorMessage(undefined);
+    }
     const {
       data: { meals },
     } = await axios.get<SearchRecipeResponse>(`/filter.php?i=${queryString}`);
+    if (!meals) {
+      setErrorMessage('Oops, couldnt find anything!');
+    }
     setMeals(meals);
   }
 
@@ -28,6 +36,14 @@ export default function Home({ categories }: Props) {
       <Header showSearchBar queryFn={searchRecipies} />
 
       <div className='max-w-7xl mx-auto'>
+        {!meals && errorMessage && (
+          <div className='p-4'>
+            <div className='mt-6 py-2 px-4 bg-red-50 text-red-900'>
+              <p>{errorMessage}</p>
+            </div>
+          </div>
+        )}
+
         {meals && <Meals meals={meals} />}
         <Categories categories={categories} />
       </div>
